80a4e402f86010ccc2e673a907a5a0cc9a49cb21
AllanDav1d <allan.j.k15@gmail.com>
2021-09-07 23:58:13 -0300

    Conceitos do ReactQuery Final

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -13,7 +13,21 @@ export default function UserList() {
     const response = await fetch('http://localhost:3000/api/users')
     const data = await response.json()
 
-    return data
+    const users = data.users.map(user => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        })
+      }
+    })
+    return users
+  }, {
+    staleTime: 1000 * 5
   });
 
   console.log(data)
@@ -70,32 +84,37 @@ export default function UserList() {
                     </Tr>
                   </Thead>
                   <Tbody>
-                    <Tr>
-                      <Td px={['4', '4', '6']}>
-                        <Checkbox colorScheme='pink' />
-                      </Td>
-                      <Td>
-                        <Box>
-                          <Text fontWeight='bold'> Allan David de Oliveira</Text>
-                          <Text fontSize='sm' color='gray.300'> allan.j.k15@gmail.com</Text>
-                        </Box>
-                      </Td>
-                      {isWideVersion && <Td>
-                        04 de Abril, 2021
-                      </Td>}
-                      <Td>
-                        <Button
-                          as='a'
-                          size='sm'
-                          fontSize='sm'
-                          colorScheme='purple'
-                          px={['1', '1', '3']}
-                          leftIcon={<Icon as={RiPencilLine} fontSize='16' ms={['2', '2', '0']} />}
-                        >
-                          {isWideVersion && 'Editar'}
-                        </Button>
-                      </Td>
-                    </Tr>
+                    {data.map(user => {
+                      return (
+                        <Tr key={user.id}>
+                          <Td px={['4', '4', '6']}>
+                            <Checkbox colorScheme='pink' />
+                          </Td>
+                          <Td>
+                            <Box>
+                              <Text fontWeight='bold'> {user.name}</Text>
+                              <Text fontSize='sm' color='gray.300'>{user.email}</Text>
+                            </Box>
+                          </Td>
+                          {isWideVersion && <Td>
+                            {user.createdAt}
+                          </Td>}
+                          <Td>
+                            <Button
+                              as='a'
+                              size='sm'
+                              fontSize='sm'
+                              colorScheme='purple'
+                              px={['1', '1', '3']}
+                              leftIcon={<Icon as={RiPencilLine} fontSize='16' ms={['2', '2', '0']} />}
+                            >
+                              {isWideVersion && 'Editar'}
+                            </Button>
+                          </Td>
+                        </Tr>
+
+                      )
+                    })}
                   </Tbody>
 
                 </Table>
